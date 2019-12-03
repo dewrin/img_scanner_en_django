6839a47d92f61879cc6b2ff04ee9ee1096d8ded1
dewrin <www.dewrin@gmail.com>
2019-12-03 03:48:16 -0800

    Update views.py

--- scan/views.py
+++ scan/views.py
@@ -4,6 +4,32 @@ from django.core.files.storage import FileSystemStorage
 
 from PIL import Image
 from pytesseract import image_to_string
+from django.shortcuts import render
+from django.http import HttpResponse
+import json
+import requests, time
+
+def index(request): 
+	response_data = {}
+	response_data["success"] = True
+	response_data["url"] = None
+	response_data["code"] = None
+	if request.method == "GET":
+		try:
+			url = request.GET["url"]
+			response_data["url"] = url
+			r = requests.get(url)
+			file = "%s"%time.time()
+			with open(file, "wb") as code:
+				code.write(r.content)
+			im = Image.open(file)
+			text = image_to_string(im)
+			response_data["code"] = text
+			response_data["message"] = file
+			os.remove(file)
+		except Exception as e:
+			response_data["message"]="%s"%e
+	return HttpResponse(json.dumps(response_data),content_type="application/json")
 
 
 class IMGView(View):
